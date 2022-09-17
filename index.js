0ebc8297cfd40d8581defb26667c906b1d8e3d7f
Patricio Gómez <hardpro.ng@gmail.com>
2022-09-17 00:56:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const snmp = require('net-snmp');
+const { Telegraf } = require('telegraf')
+const oidCode = require('./oidCode')
+
+
+
+const bot = new Telegraf('5687778700:AAH0gQjZsUjXFD60L8aI3UT8Kew_gsv2Ajg')
+let upTime = 0;
+let trapServStatus = false; 
+let showTrap = false // by defect the traps are not showed
+let showHeartbeat = false; // by defect heartbeat is off 
+
+let sendMessage = function(text,ctx) {
+    try {
+        let buffer = [];
+        
+        bot.telegram.sendMessage(ctx.chat.id ,text)
+    } 
+    catch(e) 
+    {
+        console.error(e);
+    }
+};
+
+
+ function snmpTrap (ctx) {
+    var options = {
+        port: 162,
+        disableAuthorization: true,
+        includeAuthentication: false,
+        accessControlModelType: snmp.AccessControlModelType.None,
+        engineID: "8000B98380XXXXXXXXXXXXXXXXXXXXXXXX", // where the X's are random hex digits
+        address: null,
+        transport: "udp4"
+    };
+
+    sendMessage(`Listen on: ${options.addr}: ${options.port} ${options.transport} `,ctx);
+    
+    let callback = function (error, notification) {
+        if ( error ) {
+            console.log('---error recived ---')
+            console.error (error);
+            sendMessage(error,ctx)
+    
+        } else {
+            console.log('---Recived SNMP Trap---')
+            //console.log (JSON.stringify(notification, null, 2));
+            trapProcessing(notification,ctx);
+        }
+
+    };
+    
+  snmp.createReceiver (options, callback);
+
+   
+}
+
+
+
+bot.start((ctx)=>{
+    
+    console.log(ctx.from);
+    console.log(ctx.chat);
+    console.log(ctx.message);
+    console.log(ctx.updateSubTypes);
+
+    ctx.reply('welcome '+ ctx.from.first_name);
+
+
+});
+bot.command(['alarmas','Alarmas'],  (ctx) =>{
+    if (trapServStatus){
+        ctx.reply('Already Listening Traps')
+    }else{
+        trapServStatus = true;
+        snmpTrap(ctx);
+    }
+});
+
+function trapProcessing (notification,ctx) {
+    let agentAddr = notification.pdu.agentAddr;
+    let oid = notification.pdu.varbinds[0].oid;
+    let value =notification.pdu.varbinds[0].value;    
+    let textAlarm = oidCode.oidFunction(oid);
+   if (textAlarm !== 'Beacon...'){
+    
+    if (textAlarm) {
+        console.log(`OID recived: ${textAlarm} from ${agentAddr}`);
+        sendMessage(`OID recived: ${textAlarm} from ${agentAddr}`,ctx);
+    }else{
+        console.log(`Unknown OID recived: ${oid} from ${agentAddr}`);
+        sendMessage(`Unknown OID recived: ${oid} from ${agentAddr}`,ctx);
+
+    }
+
+   } else {
+    console.log(textAlarm);
+    upTime = 5*value;
+    console.log(`Uptime: ${upTime} Min.`);
+   }
+
+    
+    console.log(`Value recived: ${value}`);
+                   
+
+}
+ 
+
+console.log('Snmp Telegram Bot: STARTED');
+bot.launch()
---
+++ oidCode.js
@@ -0,0 +1,84 @@
+const oidFunction = function (oid){
+    switch(oid){
+
+    case '1.3.6.1.4.1.26061.10.8.1.1': return 'Rec1: General Error';   
+    case '1.3.6.1.4.1.26061.10.8.2.1': return 'Rec2: General Error'; 
+    case '1.3.6.1.4.1.26061.10.8.3.1': return 'Rec3: General Error'; 
+
+    case '1.3.6.1.4.1.26061.10.8.1.2': return 'Rec1: Input Low AC';
+    case '1.3.6.1.4.1.26061.10.8.2.2': return 'Rec2: Input Low AC';
+    case '1.3.6.1.4.1.26061.10.8.3.2': return 'Rec3: Input Low AC';
+
+    case '1.3.6.1.4.1.26061.10.8.1.3': return 'Rec1: Temp Sensor Fail';
+    case '1.3.6.1.4.1.26061.10.8.1.3': return 'Rec2: Temp Sensor Fail';
+    case '1.3.6.1.4.1.26061.10.8.1.3': return 'Rec3: Temp Sensor Fail';
+
+    case '1.3.6.1.4.1.26061.10.8.1.4': return 'Rec1: Temp High';
+    case '1.3.6.1.4.1.26061.10.8.2.4': return 'Rec2: Temp High';
+    case '1.3.6.1.4.1.26061.10.8.3.4': return 'Rec3: Temp High';
+    
+    case '1.3.6.1.4.1.26061.10.8.1.5': return 'Rec1: Temp Low';
+    case '1.3.6.1.4.1.26061.10.8.2.5': return 'Rec2: Temp Low';
+    case '1.3.6.1.4.1.26061.10.8.3.5': return 'Rec3: Temp Low';
+
+    case '1.3.6.1.4.1.26061.10.8.1.6': return 'Rec1: Except Temp High';
+    case '1.3.6.1.4.1.26061.10.8.2.6': return 'Rec2: Except Temp High';
+    case '1.3.6.1.4.1.26061.10.8.3.6': return 'Rec3: Except Temp High';
+
+    case '1.3.6.1.4.1.26061.10.8.1.7': return 'Rec1: Except Temp Low';
+    case '1.3.6.1.4.1.26061.10.8.2.7': return 'Rec2: Except Temp Low';
+    case '1.3.6.1.4.1.26061.10.8.3.7': return 'Rec3: Except Temp Low';
+
+    case '1.3.6.1.4.1.26061.10.8.1.8': return 'Rec1: Element Loss/Start	';
+    case '1.3.6.1.4.1.26061.10.8.2.8': return 'Rec2: Element Loss/Start	';
+    case '1.3.6.1.4.1.26061.10.8.3.8': return 'Rec3: Element Loss/Start	';
+    
+    case '1.3.6.1.4.1.26061.10.8.1.9': return 'Rec1: Input High (AC)';
+    case '1.3.6.1.4.1.26061.10.8.2.9': return 'Rec2: Input High (AC)';
+    case '1.3.6.1.4.1.26061.10.8.3.9': return 'Rec3: Input High (AC)';
+
+    case '1.3.6.1.4.1.26061.10.8.1.10': return 'Rec1: AC UVP';
+    case '1.3.6.1.4.1.26061.10.8.2.10': return 'Rec2: AC UVP';
+    case '1.3.6.1.4.1.26061.10.8.3.10': return 'Rec3: AC UVP';
+
+    case '1.3.6.1.4.1.26061.10.8.1.11': return 'Rec1: AC OVP';
+    case '1.3.6.1.4.1.26061.10.8.2.11': return 'Rec2: AC OVP';
+    case '1.3.6.1.4.1.26061.10.8.3.11': return 'Rec3: AC OVP';
+
+    case '1.3.6.1.4.1.26061.10.8.1.12': return 'Rec1: DC OVP';
+    case '1.3.6.1.4.1.26061.10.8.2.12': return 'Rec2: DC OVP';
+    case '1.3.6.1.4.1.26061.10.8.3.12': return 'Rec3: DC OVP';
+    
+    case '1.3.6.1.4.1.26061.10.8.1.13': return 'Rec1: DC UVP';
+    case '1.3.6.1.4.1.26061.10.8.2.13': return 'Rec2: DC UVP';
+    case '1.3.6.1.4.1.26061.10.8.3.13': return 'Rec3: DC UVP';
+
+    case '1.3.6.1.4.1.26061.10.8.1.14': return 'Rec1: Over Temp Protect';
+    case '1.3.6.1.4.1.26061.10.8.2.14': return 'Rec2: Over Temp Protect';
+    case '1.3.6.1.4.1.26061.10.8.3.14': return 'Rec3: Over Temp Protect';
+
+    case '1.3.6.1.4.1.26061.10.8.1.15': return 'Rec1: Input Low Major';
+    case '1.3.6.1.4.1.26061.10.8.2.15': return 'Rec2: Input Low Major';
+    case '1.3.6.1.4.1.26061.10.8.3.15': return 'Rec3: Input Low Major';
+
+    case '1.3.6.1.4.1.26061.10.8.1.16': return 'Rec1: Input High Major';
+    case '1.3.6.1.4.1.26061.10.8.2.16': return 'Rec2: Input High Major';
+    case '1.3.6.1.4.1.26061.10.8.3.16': return 'Rec3: Input High Major';
+
+    case '1.3.6.1.4.1.26061.10.8.1.17': return 'Rec1: Fan Failure';
+    case '1.3.6.1.4.1.26061.10.8.2.17': return 'Rec2: Fan Failure';
+    case '1.3.6.1.4.1.26061.10.8.3.17': return 'Rec3: Fan Failure';
+
+    case '1.3.6.1.4.1.26061.10.8.1.18': return 'Rec1: Power Limit';
+    case '1.3.6.1.4.1.26061.10.8.2.18': return 'Rec2: Power Limit';
+    case '1.3.6.1.4.1.26061.10.8.3.18': return 'Rec3: Power Limit';
+
+    // OID alarmas PDA
+
+    case '1.3.6.1.4.1.26061.10.8.13' : return 'Beacon...';
+
+    // si no esta definida devuelve la misma
+   // default: return oid ;
+    }
+} 
+exports.oidFunction = oidFunction;
